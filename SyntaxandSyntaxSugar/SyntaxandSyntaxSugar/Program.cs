aaaa98490642f51a37e984e2f0c70abe6a00e6ac
LaKiesha Hall <LaKiesha.Hall@Gmail.Com>
2022-08-11 14:37:06 -0400

    initial commit

---
+++ SyntaxandSyntaxSugar/SyntaxandSyntaxSugar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SyntaxandSyntaxSugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var answer = 4; /*inferred*/
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine"; /*Ternary if else*/
+            Console.WriteLine(response); /*replies to what the user entered*/
+           
+        }
+    }
+}
